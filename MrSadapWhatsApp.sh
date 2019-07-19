16044156e33b21009454016aa4d496f59b8163a5
R4M4D4N1 <52308821+R4M4D4N1@users.noreply.github.com>
2019-07-19 10:24:25 +0700

    Add files via upload

---
+++ MrSadapWhatsApp.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#Whatsapp 081615788876
+#By Mr R4M4 D4N1 
+#SMK YP 17 SELOREJO
+#IG rama_dani2001
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $blue "<|            Tools SadapWhatsApp       |>"
+echo  $blue "<|                                      |>"
+echo  $blue "<| Name: Mr.R4M4 D4N1    ||    Mr.R4M4D4N1   |>"
+echo  $blue "<| MyTeam: SMK YP 17 SELOREJO         |>"
+echo  $blue "<| Contact: +6281615788876             |>"
+echo  $blue "<| Thanks To: Mr.R4M4 D4N1 Cyber Team      |>"
+echo  $blue "<|======================================|>"
+sleep 1
+echo  $red "============== $red MENU $green ================="
+echo $blue "================================================="
+echo $blue "|| $green 1. Sadap WhatsApp No Indosat   $blue ||"
+echo $blue "|| $green 2. Sadap WhatsApp No Telkomsel $blue ||"
+echo $blue "|| $green 3. Sadap WhatsApp No XL / Axis $blue ||"
+echo $blue "|| $green 4. Sadap WhatsApp No 3(Three)  $blue ||"
+echo $blue "|| $green 0. Exit Sadap WhatsApp         $blue ||"
+echo $blue "================================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@Mr.R4M4D4N1=•>" R4M4D4N1;
+if [ $MrR4M4 = 1 ] || [ $MrR4M4 = 01 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrR4M4D4N1=•>" MrR4M4;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 3
+echo $blue "10%"
+sleep 4
+echo $green "20%"
+sleep 2
+echo $cyan "30%"
+sleep 8
+echo $purple "40%"
+sleep 3
+echo $blue "50%"
+sleep 2
+echo $red "60%"
+sleep 5
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 3
+echo $blue "100%"
+sleep 2
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Verifikasi"fi
+fi
+
+if [ $MrR4M4 = 2 ] || [ $MrR4M4 = 02 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MR.R4M4=•>" MrR4M4;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 5
+echo $blue "10%"
+sleep 3
+echo $green "20%"
+sleep 8
+echo $cyan "30%"
+sleep 3
+echo $purple "40%"
+sleep 6
+echo $blue "50%"
+sleep 5
+echo $red "60%"
+sleep 4
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 8
+echo $purple "90%"
+sleep 6
+echo $blue "100%"
+sleep 9
+echo $green "Prosessing Done"
+sleep 3
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Verifikasi"
+fi
+
+if [ $MrR4M4 = 3 ] || [ $MrR4M4 = 03 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrR4M4=•>" MrR4M4;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "10%"
+sleep 4
+echo $green "20%"
+sleep 5
+echo $cyan "30%"
+sleep 6
+echo $purple "40%"
+sleep 4
+echo $blue "50%"
+sleep 2
+echo $red "60%"
+sleep 3
+echo $green "70%"
+sleep 9
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 4
+echo $blue "100%"
+sleep 9
+echo $green "Prosessing Done"
+sleep 9
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Nomer $Kode Verifikasi"
+fi
+
+if [ $MrR4M4 = 4 ] || [ $MrR4M4 = 04 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrR4M4=•>" MrR4M4;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "10%"
+sleep 7
+echo $green "20%"
+sleep 5
+echo $cyan "30%"
+sleep 4
+echo $purple "40%"
+sleep 5.5
+echo $blue "50%"
+sleep 4
+echo $red "60%"
+sleep 5
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 5
+echo $blue "100%"
+sleep 3
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Nomer $Kode Verifikasi"
+fi
+
+if [ $MrR4M4 = 0 ] || [ $MrR4M4 = 00 ]
+then
+echo  $red "Bye Bye Sayang"
+echo  $blue "Ojo Lali Kudu Ngewei Lek Iso!"
+echo  $green "MrR4M4D4N1 Cyber Team!"
+exit
+fi
+
